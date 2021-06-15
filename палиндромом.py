7e48fc3412f94575470881508b81aa8619b3ab3b
Vladislav118 <74978882+Vladislav118@users.noreply.github.com>
2021-06-15 22:30:34 +0300

    Delete палиндромом.py

--- палиндромом.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-for i in range(a, b + 1):
-    x = i // 1000
-    y = i // 100 % 10
-    z = i // 10 % 10
-    p = i % 10
-    if x == p and y == z:
-        print(i)
